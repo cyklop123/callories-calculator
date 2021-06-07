e09cef32f9e6f394d76829366b6dfd97e3296ac4
cyklop123 <irekbiela@gmail.com>
2021-06-07 12:05:52 +0200

    Add date picker

--- src/containers/DashboardPage.js
+++ src/containers/DashboardPage.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
+import moment from 'moment'
 import {useEffect, useState} from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 import UserProduct from '../components/UserProduct'
 import Summary from '../components/Summary'
+import DatePicker from '../components/DatePicker'
 
 const DashboardPage = () => {
 
@@ -14,24 +16,51 @@ const DashboardPage = () => {
     })
 
     const [userProducts, setUserProducts] = useState(null)
+    const [date, setDate] = useState(null)
     
     useEffect(() => {
-        axiosInstance.get('/'+new Date().toISOString().split('T')[0])
-        .then(res => {
-            setUserProducts(res.data)
-        })
-        .catch(err => {
-            if(err.response){
-                console.log(err.response.code)
-                setUserProducts(null)
-            }
-        })
+        const today = new Date().toISOString().split('T')[0]
+        getAllProduct(today)
     }, [])
+
+    const prevDay = (e) => {
+        e.preventDefault()
+    
+        let dateParent = moment(date)
+        dateParent.set({hour: 0, minute: 0, second: 0, millisecond: 0})
+        const yesterday = dateParent.subtract(1, 'd').format('YYYY-MM-DD')
+        getAllProduct(yesterday)
+    }
+
+    const nextDay = (e) => {
+        e.preventDefault()
+
+        let dateParent = moment(date)
+        dateParent.set({hour: 0, minute: 0, second: 0, millisecond: 0})
+        const tomorrow = dateParent.add(1, 'd').format('YYYY-MM-DD')
+        getAllProduct(tomorrow)
+    }
+
+    const getAllProduct = day => {
+        console.log(day)
+        axiosInstance.get('/'+day)
+            .then(res => {
+                setUserProducts(res.data)
+                setDate(day)
+            })
+            .catch(err => {
+                if(err.response){
+                    console.log(err.response.code)
+                    setUserProducts(null)
+                }
+            })
+    }
 
     return (
         <>
         <Row>
             <Col>
+                <DatePicker clickLeft={prevDay} clickRight={nextDay} date={date} />
                 {userProducts && <UserProduct userProducts={userProducts.userProducts} /> }
             </Col>
         </Row>
